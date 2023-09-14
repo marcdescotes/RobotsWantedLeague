1ccf2b7b1edd156ca6739f32770d9d5aeea9b1f7
didier amyot <didier@nimblerx.com>
2023-09-14 09:18:51 -0400

    First commit

---
+++ RobotsWantedLeague/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RobotsWantedLeague.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
