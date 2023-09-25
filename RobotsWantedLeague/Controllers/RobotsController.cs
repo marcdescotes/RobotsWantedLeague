dcfbc98cbd16b468fce62d4d40f23f54bd7ccc6c
Marc-André Descôteaux <125318411+marcdescotes@users.noreply.github.com>
2023-09-25 10:52:52 -0400

    Added list of countries + toLower

--- RobotsWantedLeague/Controllers/RobotsController.cs
+++ RobotsWantedLeague/Controllers/RobotsController.cs
@@ -1,8 +1,7 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using RobotsWantedLeague.Models;
 using RobotsWantedLeague.Services;
-using System.ComponentModel.DataAnnotations;
+
 
 namespace RobotsWantedLeague.Controllers;
 
@@ -79,41 +78,41 @@ public class RobotsController : Controller
 
 
 
-[HttpPost]
-public IActionResult ChangeRobotCountry(int robotId, string newCountry)
-{
-    // Liste des pays valides
-    string[] validCountries = { "Canada", "USA", "Mexico", "Brésil" };
-
-    var viewModel = new ChangeRobotCountryViewModel { NewCountry = newCountry };
-
-    // Vérifie si le pays est valide
-    if (!validCountries.Contains(newCountry))
+    [HttpPost]
+    public IActionResult ChangeRobotCountry(int robotId, string newCountry)
     {
-        // Lancer une erreur si le pays n'est pas valide
-        viewModel.ErrorMessage = "Le pays n'est pas valide. Les pays valides sont : Canada, USA, Mexico, Brésil";
-        return View("Robot", viewModel); // nom de la vue
+        // Liste des pays valides
+        string[] validCountries = {
+"Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua & Deps", "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo", "Congo", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Ivory Coast", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "North Korea", "South Korea", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Russia", "Rwanda", "St Kitts & Nevis", "St Lucia", "Saint Vincent & the Grenadines", "Samoa", "San Marino", "Sao Tome & Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Togo", "Tonga", "Trinidad & Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican", "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe"
+};
+
+        var viewModel = new ChangeRobotCountryViewModel { NewCountry = newCountry };
+
+        // Vérifie si le pays est valide
+        if (!validCountries.Contains(char.ToUpper(newCountry[0]) + newCountry.Substring(1)))
+        {
+            // Lancer une erreur si le pays n'est pas valide
+            viewModel.ErrorMessage = "Le pays n'est pas valide.";
+            return View("Robot", viewModel); // nom de la vue
+        }
+
+
+        newCountry = char.ToUpper(newCountry[0]) + newCountry.Substring(1);
+
+
+        // Si le pays est valide, rouler les fonctions
+        try
+        {
+            robotsService.ChangeRobotCountry(robotId, newCountry);
+            return RedirectToAction("Robot", new { id = robotId });
+        }
+        catch (Exception ex)
+        {
+            // Gestion des erreurs
+            viewModel.ErrorMessage = "Une erreur s'est produite lors de la modification du pays du robot.";
+
+            return View("Robot", viewModel);
+        }
     }
-
-    // Si le pays est valide, rouler les fonctions
-    try
-    {
-        robotsService.ChangeRobotCountry(robotId, newCountry);
-        return RedirectToAction("Robot", new { id = robotId });
-    }
-    catch (Exception ex)
-    {
-        // Gestion des erreurs
-        viewModel.ErrorMessage = "Une erreur s'est produite lors de la modification du pays du robot.";
-
-        return View("Robot", viewModel); // Nom de la vue
-    }
-}
-
-
-
-
-
-
 
 }
